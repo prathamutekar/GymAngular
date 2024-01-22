28e5fd6606c8a30802ac63c6c9c62bcca6fba406
unknown <prathamuttekar02@gmail.com>
2024-01-22 15:03:49 +0530

    user panel almost done

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { GlobalService } from '../global.service';
+
+declare var $: any; // Declare jQuery
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
+  providers: [MessageService]
 })
 export class GalleryComponent {
+  galleryImages: any = [];
 
+  constructor(private router: Router, private messageService: MessageService, private global: GlobalService) { }
+
+  ngAfterViewInit() {
+    this.getGalleryImages();
+  }
+
+  getGalleryImages() {
+    this.global.get(this.global.basepath + '/getGalleryImage').subscribe((res: any) => {
+      this.galleryImages = res.data;
+
+      // Initialize Magnific Popup after getting gallery images
+      this.initializeMagnificPopup();
+    });
+  }
+
+  initializeMagnificPopup() {
+    $('#dynamicGallery').magnificPopup({
+      delegate: 'a.image-popup',
+      type: 'image',
+      gallery: {
+        enabled: true
+      },
+      zoom: {
+        enabled: true,
+        duration: 300,
+        opener: function (element: any) {
+          return element.find('i');
+        }
+      }
+    });
+  }
 }
--- src/app/our-team/our-team.component.ts
+++ src/app/our-team/our-team.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-our-team',
   templateUrl: './our-team.component.html',
-  styleUrls: ['./our-team.component.css']
+  styleUrls: ['./our-team.component.css'],
+  providers: [MessageService]
 })
 export class OurTeamComponent {
 
+  trainers: any = []
+  constructor(private fb: FormBuilder, private router: Router, private messageService: MessageService, private global: GlobalService) { }
+
+  ngOnInit() {
+    this.getTrainers()
+    
+  }
+
+  getTrainers() {
+    this.global.get(this.global.basepath + '/getTrainer').subscribe((res: any) => {
+      this.trainers = res.data;
+    })
+  }
 }
